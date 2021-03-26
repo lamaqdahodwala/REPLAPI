6f521196bc9f228af8cfd52918cf527acf56c35c
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-03-26 19:05:34 -0400

    stuff

--- App/REPLAPI/main.py
+++ App/REPLAPI/main.py
@@ -1,10 +1,33 @@
-import asyncio
-import aiohttp
+import requests
 from bs4 import BeautifulSoup
+from errors.errors import UserNotFoundError
+
 
 class Client():
-	async def __init__(self, username):
+	def __init__(self, username):
 		self.username = username
-		self.sess = aiohttp.ClientSession() 
-		
-  
+		try:
+			resp = requests.get('https://replit.com/data/profiles/{}'.format(self.username))
+			if resp.ok():
+				self.json = resp.json()
+			else:
+				raise Exception
+		except Exception:
+			raise UserNotFoundError('User not found.')
+
+	def bio(self):
+		return self.json['bio']			
+	
+	def top_langs(self):
+		return self.json['topLanguages']
+	
+	def newest_repls(self):
+		return self.json['repls']
+	
+	
+
+
+if __name__ == '__main__':
+	client = Client('LAMAQDAHODWALA')
+	print(client.bio())
+			
